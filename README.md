45ab899222f19f8995f9784aa96c5d3ed2d570b7
Owen Feik <owenhfeik@gmail.com>
2023-04-24 12:59:53 +1000

    source co streams

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # TODO
 
-* https://github.com/thomasasfk/clipsync/blob/main/_twitch/queries.py
+* Find match start time within vod
+* Filter for streamers showing VALORANT
+* Show streamers under streams (if currently streaming valorant)
+* Option to add keyword filters (costream, co-stream, vs, etc)
+* Chrome extension
--- firefox/script.js
+++ firefox/script.js
@@ -1,3 +1,59 @@
+// source: https://github.com/thomasasfk/clipsync/blob/main/config.py
+const TWITCH_GQL_CONFIG = {
+    url: "https://gql.twitch.tv/gql",
+    client_id: "kimne78kx3ncx6brgo4mv6wki5h1ko",
+};
+
+/**
+ * Perform a GQL query using the Twitch GQL API.
+ * 
+ * @param {String} gql GQL query. 
+ * @param {Object} vars Variable map. 
+ * @param {JSON => void} callback Handler for returned JSON.
+ */
+function twitchGqlQuery(gql, vars, callback) {
+    fetch(TWITCH_GQL_CONFIG.url, {
+        headers: { "Client-Id": TWITCH_GQL_CONFIG.client_id },
+        method: "POST",
+        body: JSON.stringify({
+            "query": gql,
+            "variables": vars,
+        }),
+    }).then(resp => resp.json().then(callback))
+}
+
+/**
+ * Query Latest 100 vods 
+ * 
+ * @param {String[]} streamers Streamer login names.
+ * @param {Object[] => void} callback Handler for returned JSON data. 
+ */
+function getLatestStreamerVods(streamers, callback) {
+    const QUERY = `
+    query($logins: [String!]) {
+        users(logins: $logins) {
+            login
+            videos(first: 100) {
+                edges {
+                    node {
+                        id
+                        createdAt
+                        lengthSeconds
+                    }
+                    cursor
+                }
+            }
+        }
+    }
+    `;
+
+    twitchGqlQuery(
+        QUERY,
+        { logins: streamers },
+        resp => { callback(resp["data"]); }
+    );
+}
+
 function addStreamLink(text, url) {
     const VOD_LIST_SELECTOR = ".match-vods .match-streams-container";
 
@@ -27,8 +83,71 @@ function createStreamLink(text, url) {
     return streamLink;
 }
 
+/**
+ * Sourced from the time in top left of the match info box.
+ * @returns {number} Start time of the match, UTC millis.
+ */
+function getMatchTime() {
+    const SELECTOR = ".match-header-date .moment-tz-convert";
+    const ATTRIBUTE = "data-utc-ts";
+    const VLR_TZ = "EDT"; // VLR dates are in EDT not UTC
+
+    let string = document.querySelector(SELECTOR).getAttribute(ATTRIBUTE);
+    let date = new Date(string);
+    let [_w, month, day, year, time] = date.toString().split(" ");
+    let format = [day.padStart(2, "0"), month, year, time, VLR_TZ].join(" ");
+    return new Date(format).getTime();
+}
+
+/**
+ * Finds a list of vods and calls callback with an array of { streamer, vodId }
+ * objects.
+ *  
+ * @param {Object[] => void} callback Handler for vod list. 
+ */
+function findCoStreams(callback) {
+    let streamers = ["sliggytv", "sideshow"];
+
+    let matchTime = getMatchTime();
+    getLatestStreamerVods(streamers, data => {
+        let vods = [];
+
+        console.log(data);
+        data["users"].forEach(entry => {
+            let vodId = null;
+            entry["videos"]["edges"].forEach(edge => {
+                let node = edge["node"];
+                let streamTime = new Date(node["createdAt"]).getTime();
+                let length = node["lengthSeconds"] * 1000; // s to ms
+                let startsBefore = streamTime <= matchTime;
+                let endsAfter = streamTime + length >= matchTime; 
+                if (startsBefore && endsAfter) {
+                    vodId = node["id"];
+                }
+            });
+    
+            if (vodId != null) {
+                vods.push({ streamer: entry["login"], vodId });
+            }
+        });
+    
+        callback(vods);
+    });
+}
+
+/**
+ * Create a link for each co stream found.
+ */
+function addCoStreamLinks() {
+    const URL_PREFIX = "https://www.twitch.tv/videos/";
+
+    findCoStreams(vods => vods.forEach(
+        vod => addStreamLink(vod.streamer, URL_PREFIX + vod.vodId)
+    ));
+}
+
 function main() {
-    addStreamLink("Owen's Website", "https://owen.feik.xyz/");
+    addCoStreamLinks();
 }
 
 main();
